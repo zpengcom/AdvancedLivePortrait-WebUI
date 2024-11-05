cb528cc74a985152654213370920dd4a1919b99f
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-11-05 19:57:51 +0900

    Add model paths

--- modules/utils/paths.py
+++ modules/utils/paths.py
@@ -6,6 +6,13 @@ PROJECT_ROOT_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), ".."
 MODELS_DIR = os.path.join(PROJECT_ROOT_DIR, "models")
 OUTPUTS_DIR = os.path.join(PROJECT_ROOT_DIR, "outputs")
 MODEL_CONFIG = os.path.join(PROJECT_ROOT_DIR, "modules", "config", "models.yaml")
+MODEL_PATHS = {
+    "appearance_feature_extractor": os.path.join(MODELS_DIR, "appearance_feature_extractor.safetensors"),
+    "motion_extractor": os.path.join(MODELS_DIR, "motion_extractor.safetensors"),
+    "warping_module": os.path.join(MODELS_DIR, "warping_module.safetensors"),
+    "spade_generator": os.path.join(MODELS_DIR, "spade_generator.safetensors"),
+    "stitching_retargeting_module": os.path.join(MODELS_DIR, "stitching_retargeting_module.safetensors")
+}
 
 
 @functools.lru_cache
