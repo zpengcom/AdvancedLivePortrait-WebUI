71b7d31ddc9f50e81f9113fe7c134c24d7c2fa86
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-11-05 19:27:59 +0900

    Add path

--- modules/utils/paths.py
+++ modules/utils/paths.py
@@ -4,12 +4,14 @@ import os
 
 PROJECT_ROOT_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), "..", "..")
 MODELS_DIR = os.path.join(PROJECT_ROOT_DIR, "models")
+OUTPUTS_DIR = os.path.join(PROJECT_ROOT_DIR, "outputs")
 
 
 @functools.lru_cache
 def init_dirs():
     for dir_path in [
-        MODELS_DIR
+        MODELS_DIR,
+        OUTPUTS_DIR
     ]:
         os.makedirs(dir_path, exist_ok=True)
 
