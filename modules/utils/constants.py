744a72fb431cdd81ae427804c722bc9db367d42d
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-11-07 01:22:28 +0900

    Update header on space

--- modules/utils/constants.py
+++ modules/utils/constants.py
@@ -17,6 +17,8 @@ class SamplePart(Enum):
 
 REPO_MARKDOWN = """
 ## [AdvancedLivePortrait-WebUI](https://github.com/jhj0517/AdvancedLivePortrait-WebUI/tree/master)
+
+The space runs on the CPU, so it's slow. If you want faster inference, try running it locally using the link above. 
 """
 
 GRADIO_CSS = """
